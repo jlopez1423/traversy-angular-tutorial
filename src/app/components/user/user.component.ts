8307a7aebbc18b89e06d2ee0470c66b9f21cbac2
Jose Lopez <jalopez1423@gmail.com>
2018-09-12 12:02:29 -0700

    working with buttons, types, on initialize and constructors

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  hobbies: string[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('constructor ran...');
   }
 
+  ngOnInit() {
+    console.log('ngOnInit ran...');
+    this.name = 'John Doe';
+    this.age = 30;
+    this.address = {
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA'
+    };
+    this.hobbies = ['write code', 'watch movies', 'Listen to music'];
+  }
+
+  onClick() {
+    this.name= 'Jose Lopez';
+    this.hobbies.push('New Hobby');
+  }
+
+}
+
+interface Address {
+  street: string;
+  city: string;
+  state: string;
 }
